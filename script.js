6d6ec649fc98d8cbce135b4036b5be9c73360b2f
Christopher Rojas <christopherrojas@Christophers-iMac.local>
2020-08-14 15:05:54 -0700

    local storage

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
 $(document).ready(function () {
-    let time = moment().format('h a')
-    let now = moment().format('dddd, MMMM Do');
-    console.log(time);
-    console.log(now);
 
-      $('#col-1').text(time).addClass('text-secondary')
+  let time = moment().format('h a')
+    let now = moment().format('dddd, MMMM Do');
+
+    // console.log(now);
+    // console.log(time);
+
+      // $('#col-1').text(time).addClass('text-secondary')
       $('#currentDay').text(now).addClass('display-4 text-light')
 
+      let $container = $('#container');
+
+      for ( let i = 9; i < 17; i++) {
+
+        let $row = $('<row>').addClass('row border-warning pb-1');
+
+        let $div1 = $('<div>').addClass('col-1 bg-light border border-left-0 rounded border-warning text-center align-items-center justify-content-center d-flex')
+        
+        $row.append($div1);
+
+        $container.append($row[i]);
+
+      };
 
 
 
-    // console.log(`${m.toString()}`);
-    // console.log(longDate);
-    // console.log(random);
-    // console.log(lt);
-    // console.log(h);
 
 
+      $('#div-end').on('click', function (event) {
+        event.preventDefault()
 
+        let setting = [];
+        let notes = $('#input').val().trim()
+        let infoSet = localStorage.setItem('9:am', JSON.stringify(notes));
+        let infoGet = JSON.parse(localStorage.getItem('9:am'));
+        if (infoGet === null) {
+          return ''
+        } else {
+          setting.push(infoGet)
+        }
+
+        console.log(setting);
+
+        $('#input').val(setting);
+      
+
+        
+      //   setting.push(infoGet);
+
+      });
 
 });
-
-
-
-
-let $container = $('#container')
-
-let $row = $('<row>');
-
-let $div1 = $('<col-1>');
-let $div10 = $('<col-10>');
-let $input = $('<input>');
-let $divEnd = $('<div-end>');
-let $btn = $('<btn>');
