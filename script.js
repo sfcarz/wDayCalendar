16adab7de7350c862432011bba357138effed121
Christopher Rojas <christopherrojas@Christophers-iMac.local>
2020-08-15 09:51:07 -0700

    Input Box

--- script.js
+++ script.js
@@ -1,53 +1,61 @@
 $(document).ready(function () {
 
-  let time = moment().format('h a')
+    let time = moment().format('h');
     let now = moment().format('dddd, MMMM Do');
-
-    // console.log(now);
-    // console.log(time);
+    let $container = $('#container');
 
       // $('#col-1').text(time).addClass('text-secondary')
-      $('#currentDay').text(now).addClass('display-4 text-light')
+      $('#currentDay').text(now).addClass('display-4 text-light');
 
-      let $container = $('#container');
 
-      for ( let i = 9; i < 17; i++) {
-
+      for ( let i = 9; i < 13; i++) {
         let $row = $('<row>').addClass('row border-warning pb-1');
 
-        let $div1 = $('<div>').addClass('col-1 bg-light border border-left-0 rounded border-warning text-center align-items-center justify-content-center d-flex')
-        
-        $row.append($div1);
+        let $div1 = $('<div>').addClass('col-1 bg-light border border-left-0 rounded border-warning text-center align-items-center justify-content-center d-flex text-secondary').text(`${i} : am`).attr('id', `hr${i}`);
 
-        $container.append($row[i]);
+        let $div10 = $('<div>').addClass('col-10 bg-info input-group');
+        let $input = $('<input>').addClass(`form-control border-danger mt-1 mb-1 p-5 text-left`).attr({type: 'text', id: `input-${i}`});
+        $div10.append($input);
+
+        let $divEnd = $('<div>').addClass('col-1 bg-warning rounded');
+        let $button = $('<button>').addClass(`btn btn-primary time-${i}`).attr({type: 'submit', id:`time-${i}`}).text('Submit');
+        $divEnd.append($button);
+
+        $row.append($div1, $div10, $divEnd);
+        $container.append($row);
+
+        let hour = $(`#hr${i}`).val();
+
+        console.log(hour);
+
+        if (`hr${i}` === time) {
+          // console.log('Time is 12am');
+        } else {
+          // console.log('Time is Never');
+        }
+
+
+        $(`.time-${i}`).on('click', function (event) {
+          event.preventDefault();
+          let storage = [];
+          let writing = $(`#input-${i}`).val().trim();
+          let setWriting = localStorage.setItem(`time: ${i}`, JSON.stringify(writing));
+          let getWriting = JSON.parse(localStorage.getItem(`time: ${i}`));
+          // console.log(getWriting);
+          if (getWriting === null) {
+            return
+          } else {
+            storage.push(getWriting)
+          }
+        });
+
 
       };
 
-
-
-
-
-      $('#div-end').on('click', function (event) {
-        event.preventDefault()
-
-        let setting = [];
-        let notes = $('#input').val().trim()
-        let infoSet = localStorage.setItem('9:am', JSON.stringify(notes));
-        let infoGet = JSON.parse(localStorage.getItem('9:am'));
-        if (infoGet === null) {
-          return ''
-        } else {
-          setting.push(infoGet)
-        }
-
-        console.log(setting);
-
-        $('#input').val(setting);
-      
-
-        
-      //   setting.push(infoGet);
-
-      });
-
 });
+
+
+
+
+
+
