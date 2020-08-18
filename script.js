af85a4828752bf28306c9283dad39e9936409f84
Christopher Rojas <christopherrojas@Christophers-iMac.local>
2020-08-18 00:22:51 -0700

    last minute notes

--- script.js
+++ script.js
@@ -1,40 +1,32 @@
 $(document).ready(function () {
-
     let time = moment().format('h:a');
+    console.log(time);
     let now = moment().format('dddd, MMMM Do');
     let $container = $('#container');
 
-      console.log(time);
-      // $('#col-1').text(time).addClass('text-secondary')
       $('#currentDay').text(now).addClass('display-4 text-light');
 
-
-      for ( let i = 9; i < 12; i++) {
+      for ( let i = 8; i < 12; i++) {
         let $row = $('<row>').addClass('row border-warning pb-1');
         let $div1 = $('<div>').addClass('col-1 border border-left-0 rounded border-dark text-center align-items-center justify-content-center d-flex text-light hour bg-secondary').text(`${i}:am`).attr('id', `hr${i}`);
-        let $div10 = $('<div>').addClass('col-10 bg-secondary input-group');
-        let $input = $('<textarea>').addClass(`form-control text-dark bg-secondary mt-1 mb-1 p-5 text-left`).attr({type: 'text', id: `input-${i}`});
+        let $div10 = $('<div>').addClass('col-10 bg-info input-group');
+        let $input = $('<textarea>').addClass(`form-control text-light bg-secondary mt-1 mb-1 p-5 text-left`).attr({type: 'text', id: `input-${i}`});
         $div10.append($input);
-        let $divEnd = $('<div>').addClass('col-1 bg-warning rounded');
-        let $button = $('<button>').addClass(`btn btn-primary saveBtn time-${i}`).attr({type: 'submit', id:`time-${i}`}).text('Submit');
+        let $divEnd = $('<div>').addClass('col-1 bg-info rounded');
+        let $button = $('<button>').addClass(`btn btn-primary saveBtn mt-5 mr-5 time-${i}`).attr({type: 'submit', id:`time-${i}`}).text('Submit');
         $divEnd.append($button);
         $row.append($div1, $div10, $divEnd);
         $container.append($row);
 
         let hour = $(`#hr${i}`).text();
-
-        // console.log(hour);
-
-        if (`hr${i}` === time) {
-          // console.log('Time is 12am');
+        if (hour == time) {
+          $(`#input-${i}`).removeClass('bg-success').addClass('bg-danger')
         } else {
-          // console.log('Time is Never');
+          $(`#input-${i}`).removeClass('bg-secondary').addClass('bg-success')
         }
-
 
         $(`.time-${i}`).on('click', function (event) {
           event.preventDefault();
-
           let writing = $(`#input-${i}`).val().trim();
           let setWriting = localStorage.setItem(`time: ${i}`, JSON.stringify(writing));
         });
@@ -48,28 +40,23 @@ $(document).ready(function () {
         let $row = $('<row>').addClass('row border-warning pb-1');
         let $div1 = $('<div>').addClass('col-1 border border-left-0 rounded border-dark text-center align-items-center justify-content-center d-flex text-light hour bg-secondary').text(`${i}:pm`).attr('id', `hr${i}`);
         let $div10 = $('<div>').addClass('col-10 bg-info input-group');
-        let $input = $('<textarea>').addClass(`form-control border-info text-dark bg-info mt-1 mb-1 p-5 text-left`).attr({type: 'text', id: `input-${i}`});
+        let $input = $('<textarea>').addClass(`form-control text-light bg-secondary mt-1 mb-1 p-5 text-left`).attr({type: 'text', id: `input-${i}`});
         $div10.append($input);
-        let $divEnd = $('<div>').addClass('col-1 bg-warning rounded');
-        let $button = $('<button>').addClass(`btn btn-primary saveBtn time-${i}`).attr({type: 'submit', id:`time-${i}`}).text('Submit');
+        let $divEnd = $('<div>').addClass('col-1 bg-info rounded');
+        let $button = $('<button>').addClass(`btn btn-primary mt-5 mr-5 saveBtn time-${i}`).attr({type: 'submit', id:`time-${i}`}).text('Submit');
         $divEnd.append($button);
         $row.append($div1, $div10, $divEnd);
         $container.append($row);
 
         let hour = $(`#hr${i}`).text();
-
-        // console.log(hour);
-
-        if (`hr${i}` === time) {
-          // console.log('Time is 12am');
+        if (hour == time) {
+          $(`#input-${i}`).removeClass('bg-success').addClass('bg-danger')
         } else {
-          // console.log('Time is Never');
+          $(`#input-${i}`).removeClass('bg-secondary').addClass('bg-success')
         }
-
 
         $(`.time-${i}`).on('click', function (event) {
           event.preventDefault();
-
           let writing = $(`#input-${i}`).val().trim();
           let setWriting = localStorage.setItem(`time: ${i}`, JSON.stringify(writing));
         });
@@ -83,28 +70,23 @@ $(document).ready(function () {
         let $row = $('<row>').addClass('row border-warning pb-1');
         let $div1 = $('<div>').addClass('col-1 border border-left-0 rounded border-dark text-center align-items-center justify-content-center d-flex text-light hour bg-secondary').text(`${i}:pm`).attr('id', `hr${i}`);
         let $div10 = $('<div>').addClass('col-10 bg-info input-group');
-        let $input = $('<textarea>').addClass(`form-control border-info text-dark bg-info mt-1 mb-1 p-5 text-left`).attr({type: 'text', id: `input-${i}`});
+        let $input = $('<textarea>').addClass(`form-control text-light bg-secondary mt-1 mb-1 p-5 text-left`).attr({type: 'text', id: `input-${i}`});
         $div10.append($input);
-        let $divEnd = $('<div>').addClass('col-1 bg-warning rounded');
-        let $button = $('<button>').addClass(`btn btn-primary saveBtn time-${i}`).attr({type: 'submit', id:`time-${i}`}).text('Submit');
+        let $divEnd = $('<div>').addClass('col-1 bg-info rounded');
+        let $button = $('<button>').addClass(`btn btn-primary mt-5 mr-5 saveBtn time-${i}`).attr({type: 'submit', id:`time-${i}`}).text('Submit');
         $divEnd.append($button);
         $row.append($div1, $div10, $divEnd);
         $container.append($row);
 
         let hour = $(`#hr${i}`).text();
-
-        // console.log(hour);
-
-        if (`hr${i}` === time) {
-          // console.log('Time is 12am');
+        if (hour == time) {
+          $(`#input-${i}`).removeClass('bg-success').addClass('bg-danger')
         } else {
-          // console.log('Time is Never');
+          $(`#input-${i}`).removeClass('bg-secondary').addClass('bg-success')
         }
-
 
         $(`.time-${i}`).on('click', function (event) {
           event.preventDefault();
-
           let writing = $(`#input-${i}`).val().trim();
           let setWriting = localStorage.setItem(`time: ${i}`, JSON.stringify(writing));
         });
@@ -115,9 +97,3 @@ $(document).ready(function () {
       };
 
 });
-
-
-
-
-
-
